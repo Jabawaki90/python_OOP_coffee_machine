23a8482b514b51e4aa00efbde5a54c064d38dc90
Ashraf Azemi <ashraf.chzm@gmail.com>
2024-05-22 23:13:52 +0800

    testing turtle libraries

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print('OOP Coffee Machine')
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+my_screen = Screen()
+def f():
+    timmy.forward(50)
+
+timmy.shape('turtle')
+timmy.color("red", "green")
+timmy.forward(15)
+
+my_screen.onkey(f, "Up")
+my_screen.listen()
+
+my_screen.exitonclick()
