f0d20d6affec01cf0c04c637eb6f473d7097246f
Ashraf Azemi <ashraf.chzm@gmail.com>
2024-05-23 22:51:47 +0800

    learn pretty table

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
+#
+# timmy = Turtle()
+# my_screen = Screen()
+# def f():
+#     timmy.forward(50)
+#
+# timmy.shape('turtle')
+# timmy.color("red", "green")
+# timmy.forward(15)
+#
+# my_screen.onkey(f, "Up")
+# my_screen.listen()
+#
+# my_screen.exitonclick()
 
-timmy = Turtle()
-my_screen = Screen()
-def f():
-    timmy.forward(50)
+from prettytable import PrettyTable
 
-timmy.shape('turtle')
-timmy.color("red", "green")
-timmy.forward(15)
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu",  "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric",  "Water", "Fire"])
+table.align["Pokemon Name"] = 'l'
+table.align["Type"] = 'r'
 
-my_screen.onkey(f, "Up")
-my_screen.listen()
-
-my_screen.exitonclick()
+print(table)
